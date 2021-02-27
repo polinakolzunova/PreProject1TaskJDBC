f889d591f5d684581ee29dee9efd46cac4eb8372
Polina Kolzunova <polinakolzunova@gmail.com>
2021-02-27 19:49:54 +0300

    commit comment

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         UserService userService = new UserServiceImpl();
 
+        // new comment
         // Создание таблицы User(ов)
         userService.createUsersTable();
 
