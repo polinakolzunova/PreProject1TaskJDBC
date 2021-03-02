5b9c9dcdac7c67ab9ac4e8683308c13abd600ee0
Polina Kolzunova <polinakolzunova@gmail.com>
2021-03-02 10:26:36 +0300

    create/drop table without exceptions

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -9,6 +9,18 @@ import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
 
+    /*
+    Результат проверки ментором:
+    1. В блоке catch нужно выбрасывать исключения SQLException
+    >>> но в методах интерфейса UserDao не объявлен выброс исключений, его допустимо менять?
+    2. Где закрытие соединений в методах дао ?
+    >>> используется try-with-resource, соединение объявлено в блоке ресурсов -> автоматически закрывается
+    3. Создание таблицы для User(ов) – не должно приводить к исключению, если такая таблица уже существует
+    >>> done
+    4. Удаление таблицы User(ов) – не должно приводить к исключению, если таблицы не существует
+    >>> done
+     */
+
     public UserDaoJDBCImpl() {
     }
 
@@ -16,7 +28,7 @@ public class UserDaoJDBCImpl implements UserDao {
         try (Connection conn = Util.getMySQLConnection();
              Statement statement = conn.createStatement()) {
 
-            statement.executeUpdate("create table users(" +
+            statement.executeUpdate("create table if not exists users(" +
                     "id int auto_increment, " +
                     "name varchar(100) not null, " +
                     "lastName varchar(100) not null, " +
@@ -32,7 +44,7 @@ public class UserDaoJDBCImpl implements UserDao {
         try (Connection conn = Util.getMySQLConnection();
              Statement statement = conn.createStatement()) {
 
-            statement.executeUpdate("drop table users");
+            statement.executeUpdate("drop table if exists users");
 
         } catch (SQLException | ClassNotFoundException e) {
             System.err.println("Невозможно удалить таблицу! Message: " + e.getMessage());
@@ -90,7 +102,7 @@ public class UserDaoJDBCImpl implements UserDao {
         try (Connection conn = Util.getMySQLConnection();
              Statement statement = conn.createStatement()) {
 
-            statement.executeUpdate("truncate table users");
+            statement.executeUpdate("truncate table if exists users");
 
         } catch (SQLException | ClassNotFoundException e) {
             System.err.println("Невозможно очистить таблицу! Message: " + e.getMessage());
